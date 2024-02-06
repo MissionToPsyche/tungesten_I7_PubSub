3e79944c07743d20bdf8ee72cfcb9db2950e5687
Sudheer Reddy Kunduru <skundur1@asu.edu>
2024-02-05 23:24:22 -0700

    Functionality for adding new user

--- server/app.js
+++ server/app.js
@@ -30,6 +30,25 @@ app.post('/login', async (req, res) => {
     }
 });
 
+// Endpoint for adding a user
+app.post('/add-user', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const userExists = await User.findOne({ username });
+        if (userExists) {
+            return res.status(400).send('A user already exists with this username. Please choose a different username.');
+        }
+        const newUser = new User({
+            username,
+            password
+        });
+        await newUser.save();
+        res.status(201).send('User registered successfully');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
