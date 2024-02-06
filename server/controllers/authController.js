c74625b96424858bc121fd084b481d5f8408922e
Sudheer Reddy Kunduru <skundur1@asu.edu>
2024-02-06 10:18:02 -0700

    Updated controllers for authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,37 @@
-const userLogin = () => {};
+const User = require('../model/userModel');
+const userLogin = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).send('Authentication failed. User not found.');
+        }
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).send('Authentication failed. Wrong password.');
+        }
+        res.send('User logged in successfully');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
 
-const addUser = () => {};
+const addUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const userExists = await User.findOne({ username });
+        if (userExists) {
+            return res.status(400).send('A user already exists with this username. Please choose a different username.');
+        }
+        const newUser = new User({
+            username,
+            password
+        });
+        await newUser.save();
+        res.status(201).send('User registered successfully');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
 
 module.exports = {userLogin, addUser};
