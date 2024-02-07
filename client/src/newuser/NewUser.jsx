5fe145fb43b92951e12f7c0a4e80bdb82be4f3a4
jpate108 <jpate108@asu.edu>
2024-02-06 19:30:07 -0700

    New User page commit

---
+++ client/src/newuser/NewUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+export const NewUser = () => {
+
+
+    return (
+        <>
+            <h1>Add User</h1>
+
+        </>
+    )
+}
+
+
