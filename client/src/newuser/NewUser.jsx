8f97a86accc70e483b93490691a868ebd7b03cc3
jpate108 <jpate108@asu.edu>
2024-02-06 19:40:01 -0700

    New User page form 2nd commit

--- client/src/newuser/NewUser.jsx
+++ client/src/newuser/NewUser.jsx
@@ -1,12 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import TextField from '@mui/material/TextField';
+
 export const NewUser = () => {
 
 
     return (
         <>
             <h1>Add User</h1>
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="User Name"
+                name="Type user's name"
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="User last Name"
+                name="Type user's last name"
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="User Email"
+                name="Type user's Email"
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="Group user"
+                name="Type group of user"
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="Password"
+                name="Type user's password"
+            />
 
+            <p>*Password must have 1 Uppercase, 1 Lowercase, 1 Digit, and be a minimum of 8 characters</p>
         </>
     )
 }
