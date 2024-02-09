5afecc26c84e28b3d731bc0b1402f5ab40d0fb30
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2024-02-09 09:00:22 -0700

    feat: Add new routes and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import { Mainpage } from './components/Mainpage'
 import { MuiNavbar } from './components/Navbar'
 import Login from "./components/Login"
 import './App.css';
+import AddUser from './components/AddUser';
+import Dashboard from './components/Dashboard';
+import Publications from './components/Publications';
+import UploadDoc from './components/UploadDoc';
 
 function App() {
 
@@ -13,6 +17,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Mainpage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/adduser/" element={<AddUser />} />
+          <Route path="/dashboard/" element={<Dashboard />} />
+          <Route path="/publications/" element={<Publications />} />
+          <Route path="/uploaddoc/" element={<UploadDoc />} />
         </Routes>
       </Router>
     </div>
