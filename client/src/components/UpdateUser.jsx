24378183c3c17758f56b2cb097086232399181f6
mpate133 <mpate133@asu.edu>
2024-02-11 15:12:47 -0700

    added 3 fields in the update user form and completed the task #81

--- client/src/components/UpdateUser.jsx
+++ client/src/components/UpdateUser.jsx
@@ -11,15 +11,19 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
+
 function UpdateUser() {
 
 	const defaultTheme = createTheme();
 	const [fullName, setFullName] = useState('');
 	const [email, setEmail] = useState('');
+	const [dateOfBirth, setDateOfBirth] = useState();
 	const [showFullNameError, setFullNameErrorFlag] = useState(false);
 	var [showEmailError, setEmailErrorFlag] = useState(false);
+	var [showDateOfBirthError, setDateOfBirthErrorFlag] = useState(false);
 	var [fullNameErrorMessage, setFullNameErrorMessage] = useState('');
 	var [emailErrorMessage, setEmailErrorMessage] = useState('');
+	var [dateOfBirthErrorMessage, setDateOfBirthErrorMessage] = useState('');
 
 
 	const update = () => {
@@ -39,6 +43,12 @@ function UpdateUser() {
 		setEmail(event.target.value);
 		setEmailErrorFlag(false);
 		setEmailErrorMessage("");
+	};
+
+	const dateOfBirthChanged = (event) => {
+		// setEmail(event.target.value);
+		setDateOfBirthErrorFlag(false);
+		setDateOfBirthErrorMessage("");
 	}
 
 	return (
@@ -50,7 +60,10 @@ function UpdateUser() {
 					<Box component="form" noValidate sx={{ mt: 1 }}>
 						<TextField value={fullName} margin="normal" onChange={fullNameChanged} error={showFullNameError} required fullWidth id="fullName" helperText={fullNameErrorMessage} label="Full Name" name="fullName" />
 						<TextField value={email} margin="normal" onChange={emailChanged} error={showEmailError} required fullWidth name="email" helperText={emailErrorMessage} label="Email" type="email" id="email" />
-						<TextField value={email} margin="normal" onChange={emailChanged} error={showEmailError} required fullWidth name="date" helperText={emailErrorMessage} label="Date of Birth" type="date" id="date" />
+						<LocalizationProvider fullWidth margin="normal" dateAdapter={AdapterDayjs}>
+							<DatePicker sx={{ width: 396, marginTop: 2 }} fullWidth required margin="normal" label="Date of Birth" disableFuture />
+						</LocalizationProvider>
+						{/* <TextField value={dateOfBirth} margin="normal" onChange={dateOfBirthChanged} error={showDateOfBirthError} required fullWidth name="date" helperText={dateOfBirthErrorMessage} label="Date of Birth" type="date" id="date" /> */}
 						<Button fullWidth onClick={update} variant="contained" sx={{ mt: 3, mb: 2 }}> Update </Button>
 					</Box>
 				</Box>
