8937caed45dd882f96f2266efab818c7a79d6f3c
jpate108 <jpate108@asu.edu>
2024-02-14 15:55:09 -0700

    adding metadata schema into backend

---
+++ server/model/metaData.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const metaData = new mongoose.Schema({
+    metadataid: { type: String, required: true, unique: true },
+    documentid: { type: String, required: true },
+    metadatype: { type: String, enum: ['COMMENT', 'DISC', 'FEED'], default: 'COMMENT' }
+
+});
