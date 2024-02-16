6660efbb6ab0f7698f4080958e3d5f33972c6a9a
mpate133 <mpate133@asu.edu>
2024-02-16 12:32:47 -0700

    implemented temporary token in frontend side

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../provider/authProvider";
+import { useAuth } from "../providers/AuthProvider";
 
 export const ProtectedRoute = () => {
     const { token } = useAuth();
