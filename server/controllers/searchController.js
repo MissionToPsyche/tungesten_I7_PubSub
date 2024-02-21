54939e09ad7385019f02c9af3d961da152ff5955
Sudheer Reddy Kunduru <skundur1@asu.edu>
2024-02-21 11:28:14 -0700

    Adding the endpoint for filtering documents by title

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,11 +1,28 @@
 const Document = require("../model/documentSchema");
 
-const searchDocsByTitle = () => {
-
+const searchDocsByTitle = async (req, res) => {
+    try {
+        const { substring } = req.body;
+        const filteredDocuments = await Document.find({ title: { $regex: new RegExp(substring, 'i') } });
+        const documentsWithRelevance = filteredDocuments.map(document => {
+            const relevanceScore = calculateRelevance(document.title, substring);
+            return { document, relevanceScore };
+        });
+        const sortedDocuments = documentsWithRelevance.sort((a, b) => b.relevanceScore - a.relevanceScore).map(entry => entry.document);
+        res.json(sortedDocuments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
 
 const searchDocsByFilters = () => {
 
 }
 
-module.exports = {searchDocsByTitle, searchDocsByFilters};
+function calculateRelevance(title, substring) {
+    const matchCount = (title.match(new RegExp(substring, 'gi')) || []).length;
+    return matchCount;
+}
+
+module.exports = { searchDocsByTitle, searchDocsByFilters };
