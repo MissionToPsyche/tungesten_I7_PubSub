cb6d09afa817f18489dc39a815d3910cb193af3c
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2024-03-16 10:07:17 -0700

    US:32 Created the newTeams Component

---
+++ client/src/subComponents/Teams/NewTeams.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { Typography, Button, TextField, Stack, Autocomplete } from '@mui/material';
+
+export function NewTeam() {
+    
+
+    return (
+        <>
+            
+        </>
+    );
+}
