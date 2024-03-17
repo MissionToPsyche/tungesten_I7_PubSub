1df8a836ed3d665889ca56be04347d7d1eab6cda
mpate133 <mpate133@asu.edu>
2024-03-17 10:03:02 -0700

    Added useEffect for api call and set temp user info

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,11 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Profile() {
 
+	var [userInfo, setUserInfo] = useState({});
+
 	useEffect(() => {
 		console.log('Api call for fetching profile details goes here.');
+		setTempUserInfo();
+	}, []);
 
-	})
+	const setTempUserInfo = () => {
+		let info = {
+			name: 'Manan Patel',
+			picSvg: '',
+			dob: '12/01/1999',
+			email: 'mpate133@asu.edu',
+			totalDocContribution: 5,
+			ownedDocs: [
+				{
+					docName: 'Owned demo doc 1',
+				},
+				{
+					docName: 'Owned demo doc 2'
+				},
+				{
+					docName: 'Owned demo doc 3'
+				}
+			],
+			sharedDocs: [
+				{
+					docName: 'Shared doc with Jay'
+				},
+				{
+					dosName: 'Shared doc with Swapnil'
+				}
+			]
+		};
+
+		setUserInfo(info);
+	};
 
 	return (
 		<div>
