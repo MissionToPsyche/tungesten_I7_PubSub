501ef10127e712ece84a16be5b914a884337b5cd
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2024-03-19 08:18:10 -0700

    US:32 Call API endpoint to add teams

--- client/src/subComponents/Teams/NewTeams.jsx
+++ client/src/subComponents/Teams/NewTeams.jsx
@@ -7,10 +7,9 @@ export function NewTeam() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        // Get the users data from localStorage
+       
         const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
 
-        // Set the users state
         setUsers(storedUsers.map(user => user.username));
     }, []);
 
@@ -26,27 +25,25 @@ export function NewTeam() {
             userId: user.userId,
             role: user.role
         }));
-        // Set the teamMembers state
+        
         setTeamMembers(selectedUsersData);
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        // Get the existing teams data from localStorage
+        
         const existingTeams = JSON.parse(localStorage.getItem('teams')) || [];
 
-        // Create a new team object
+        
         const newTeam = {
             teamId: existingTeams.length + 1,
             teamName: tName,
             users: teamMembers
         };
 
-        // Add the new team to the existing teams data
         existingTeams.push(newTeam);
 
-        // Store the updated teams data in localStorage
         localStorage.setItem('teams', JSON.stringify(existingTeams));
 
         console.log("New team added: ", newTeam);
@@ -62,7 +59,6 @@ export function NewTeam() {
                     onChange={handleNameChange}
                     variant="outlined"
                     sx={{ mb: 2 }}
-                    required
                 />
                 <Stack spacing={3} sx={{ width: 500, marginBottom: 5 }}>
                     <Autocomplete
