e2df745a883117be7665070dede060c0c8ee215d
Swapnil Chadotra <schadotr@asu.edu>
2024-03-19 17:23:10 -0700

    US222 : Adding upload document and get document functions

--- server/document-service/controllers/documentActions.js
+++ server/document-service/controllers/documentActions.js
@@ -83,4 +83,107 @@ async function requestSignedUrl(req, res) {
 }
 
 
+async function uploadDocument(req, res) {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+
+    const documentTitle = "test"; // Assuming title is sent in the request body
+    const author = "abc"; // Assuming author is also provided
+
+    try {
+        // Generate and upload chunks
+        const data = Buffer.from(req.file.buffer);
+        const chunks = await generateChunks(data);
+        const chunkRefs = [];
+
+        for (let chunk of chunks) {
+            const hash = hashChunk(chunk.data);
+            let chunkRef = await Chunk.findOne({ hash });
+
+            if (!chunkRef) {
+                const s3Key = await s3Operations.uploadChunk(process.env.AWS_BUCKET_NAME, hash, chunk.data);
+                chunkRef = new Chunk({
+                    sequence: chunk.sequence,
+                    s3Key: s3Key,
+                    hash: hash,
+                    size: chunk.data.length,
+                    referenceCount: 1
+                });
+                await chunkRef.save();
+                chunkRefs.push(chunkRef._id);
+            } else {
+                chunkRef.referenceCount += 1;
+                await chunkRef.save();
+                chunkRefs.push(chunkRef._id);
+            }
+        }
+
+        // Create a new document entry with the initial version
+        const newDocument = new Document({
+            title: documentTitle,
+            author: author,
+            documentType: "txt",
+            versions: [{
+                versionNumber: '1.0',
+                chunks: chunkRefs,
+                isDraft: false,
+                changedBy: author
+            }],
+            currentVersion: '1.0',
+        });
+
+        await newDocument.save();
+
+        return res.status(201).json({
+            message: "Document uploaded successfully",
+            documentId: newDocument._id
+        });
+
+    } catch (error) {
+        console.error("Error uploading document:", error);
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+async function getDocument(req, res) {
+    const documentId = req.query.documentId;
+    const versionNumber = req.query.versionNumber;
+
+    try {
+        // Find the document by ID
+        const document = await Document.findOne({ _id: documentId });
+        if (!document) {
+            return res.status(404).json({ error: "Document not found" });
+        }
+
+        // Find the version by version number
+        const version = document.versions.find(v => v.versionNumber === versionNumber);
+        if (!version) {
+            return res.status(404).json({ error: "Version not found" });
+        }
+
+        // Retrieve the chunks for the version
+        const chunks = await Chunk.find({ _id: { $in: version.chunks } });
+        // Retrieve the chunks from S3
+        for (let chunk of chunks) {
+            const s3Object = await s3Operations.getChunk(process.env.AWS_BUCKET_NAME, chunk.hash);
+            chunk.data = s3Object;
+        }
+        // const chunkData = s3Objects.map(object => object.Body);
+        // Set the response headers
+        res.setHeader('Content-Type', 'application/octet-stream');
+        res.setHeader('Content-Disposition', `attachment; filename=${document.title}.${document.documentType}`);
+
+        // Stream the concatenated chunks
+        for (let chunk of chunks) {
+            res.write(chunk.data);
+        }
+        res.end();
+    } catch (error) {
+        console.error("Error getting document:", error);
+        return res.status(500).json({ error: error.message });
+    }
+}
+
 module.exports = { uploadDocument, getDocument }
