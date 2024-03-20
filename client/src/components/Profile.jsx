a65e995cdbce446390f76cb02c5e428b8f53e817
mpate133 <mpate133@asu.edu>
2024-03-19 20:13:02 -0700

    Added Accordian and List view In profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,21 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useEffect, useState } from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
 
 function Profile() {
-
+	const defaultTheme = createTheme();
 	var [userInfo, setUserInfo] = useState({});
 
 	useEffect(() => {
@@ -41,9 +55,50 @@ function Profile() {
 	};
 
 	return (
-		<div>
-			Profile Page
-		</div>
+		<ThemeProvider theme={defaultTheme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+					Hello
+					<Accordion>
+						<AccordionSummary
+						expandIcon={<ExpandMoreIcon />}
+						aria-controls="panel1-content"
+						id="panel1-header"
+						>
+						<Typography>Owned Documents</Typography>
+						</AccordionSummary>
+						<AccordionDetails>
+						<List>
+							{userInfo?.ownedDocs?.map((doc, i) => (<ListItem key={i} disablePadding>
+								<ListItemButton>
+								<ListItemText primary={doc.docName} />
+								</ListItemButton>
+							</ListItem>))}
+						</List>
+						</AccordionDetails>
+					</Accordion>
+					<Accordion>
+						<AccordionSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls="panel2-content"
+							id="panel2-header"
+						>
+							<Typography>Shared Documents</Typography>
+						</AccordionSummary>
+						<AccordionDetails>
+							<List>
+								<ListItem disablePadding>
+									<ListItemButton>
+									<ListItemText primary="Trash" />
+									</ListItemButton>
+								</ListItem>
+							</List>
+						</AccordionDetails>
+					</Accordion>
+				</Box>
+			</Container>
+		</ThemeProvider>
 	);
 }
 
