895b18405665ff3551617c80a7964f450061d3db
jpate108 <jpate108@asu.edu>
2024-03-21 11:33:10 -0700

    Adding button to add user in main page

--- client/src/components/Mainpage.jsx
+++ client/src/components/Mainpage.jsx
@@ -1,11 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 
 export function Mainpage() {
 
+    const navigate = useNavigate();
 
     return (
         <>
             <h1>Admin Dashboard</h1><br></br>
-            <p>Add New User</p>
+            <button onClick={() => navigate('/addUser')}>Add New User</button>
             <p>Show All Users</p>
             <p>Upload Publication</p>
             <p>Show All Publication</p>
